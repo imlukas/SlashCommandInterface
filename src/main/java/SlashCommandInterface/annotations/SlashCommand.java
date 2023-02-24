2b9ef33afc8c36fc655e4d9739cd1e335c071cd3
imlukas <lppinheiro1234@gmail.com>
2023-02-24 01:23:35 +0000

    Fixed imports

--- src/main/java/SlashCommandInterface/annotations/SlashCommand.java
+++ src/main/java/SlashCommandInterface/annotations/SlashCommand.java
@@ -1,7 +1,7 @@
 package SlashCommandInterface.annotations;
 
 
-import me.imlukas.slashcommands.CommandType;
+import SlashCommandInterface.constants.CommandType;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
